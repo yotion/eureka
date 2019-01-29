38b829d8fff1f07ca24db5a51ccbad876f8d839b
mac <yongxin.wang@yimifudao.com>
2019-01-29 18:24:45 +0800

    init

---
+++ client-1/src/main/java/com/yi/kurt/client1/controller/DcController.java
@@ -0,0 +1,19 @@
+package com.yi.kurt.client1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+
+@RestController
+public class DcController {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+}
